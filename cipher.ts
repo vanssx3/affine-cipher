4fda96d343cec3d19faa3c22da586b976a89908d
mcrangler37 <144483135+mcrangler37@users.noreply.github.com>
2023-09-09 14:22:45 -0500

    did shit

--- cipher.ts
+++ cipher.ts
@@ -1,4 +1,18 @@
 //haiii hai hihiiiii haiiiiiiiiiiiiii :3 :3 :3 :3 :3
 //^w^ ^W^
 
-let cipher:string = "";
+let cipher:string | null = prompt("encryption or decryption?");
+switch(cipher) { 
+    case "encryption": { 
+       //encrypts input; 
+       break; 
+    } 
+    case "decryption": { 
+       //decrypts input; 
+       break; 
+    } 
+    default: { 
+       console.log("error: empty return"); 
+       break; 
+    } 
+ } 
