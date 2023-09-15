975a1903c98fcd53dfbe641971224c6a70f11c17
mcrangler37 <144483135+mcrangler37@users.noreply.github.com>
2023-09-15 13:03:29 -0500

    affine

--- cipher.ts
+++ cipher.ts
@@ -2,7 +2,7 @@ let meow:string | null = prompt("encryption or decryption?");
 let input:string | null = prompt("what would you like to " + meow + "?");
 let a:int = Math.random() * 8 + 2;
 let b:int = Math.random() * 99 + 1
-let key:string = "figure out later :3"; //algorithm for genning key
+//ax+b, where x is the value of the letter in the input
 
 switch(meow) { 
     case "encryption": { 
