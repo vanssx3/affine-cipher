277453385e856010a838eaea011fa6cecad27c1d
vanssx3 <144460118+vanssx3@users.noreply.github.com>
2023-09-18 13:16:33 -0500

    mreowwww

--- affineAlgorithm.ts
+++ affineAlgorithm.ts
@@ -3,13 +3,24 @@ let alphabetLetters : string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "
 let numLetters: number = 26;
 let key = [0, 0];
 let userInput: string = "";
+let selection = number = 0;
 
 function keyGen(){
     //Generate random key to encrypt the file with
 }
 
 function input(){
-   //Prompt user to enter a string
+   selection = prompt("Enter 1 for Encryption or 2 for Decryption");
+   if(selection == (1 or 2)){
+    if(selection ==1 ){
+        console.log("Encryption Starting...")
+    }
+    if(selection == 2){
+        console.log("Decryption Starting...")
+    }
+   }
+    //Prompt user to enter a string
+   userInput = prompt("Enter a string to be encrypted or decrypted (lowercase alphabet only)" )
    //Check if string is all lowercase-letters
 }
 
