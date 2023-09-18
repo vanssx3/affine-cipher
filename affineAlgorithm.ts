ddf382b21fab7473056cc6d20e7206ff9206b4db
vanssx3 <144460118+vanssx3@users.noreply.github.com>
2023-09-18 12:57:41 -0500

    Add files via upload

---
+++ affineAlgorithm.ts
@@ -0,0 +1,18 @@
+//setup variables :3
+let alphabetLetters : string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numLetters: number = 26;
+let key = [0, 0];
+let userInput: string = "";
+
+function keyGen(){
+    //Generate random key to encrypt the file with
+}
+
+function input(){
+   //Prompt user to enter a string
+   //Check if string is all lowercase-letters
+}
+
+function encrypt(){
+    //A
+}
