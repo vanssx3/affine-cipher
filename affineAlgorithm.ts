267fb32a2fdb6d719eabb5efc5ce21217e88fa4a
vanssx3 <144460118+vanssx3@users.noreply.github.com>
2023-09-18 17:21:00 -0500

    Update affineAlgorithm.ts
    Added some keygen stuff

--- affineAlgorithm.ts
+++ affineAlgorithm.ts
@@ -2,15 +2,31 @@
 let alphabetLetters:string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let numLetters:number = 26;
 let key:number[] = [0, 0];
-let userInput:string | null = prompt("Enter a string to be encrypted or decrypted (letters only)").toLowerCase();
-let selection:number | null = prompt("Enter 1 for Encryption or 2 for Decryption");
+let userInput:string | null = prompt("Enter a string to be encrypted or decrypted (letters only)");
+let selection:any | null = prompt("Enter 1 for Encryption or 2 for Decryption");
 
 let keyDigit1:number = Math.random() * 8 + 2;
 let keyDigit2:number = Math.random() * 99 + 1;
-//ax+b, where x is the value of the letter in the input
+//Fx + S, where x is the value of the letter in the input
 
 function keyGen(){
     //create encryption key with generated values
+    switch(selection){
+        case 1:{
+            key[1] = keyDigit1;
+            key[2] = keyDigit2;
+            break;
+        }
+        case 2:{
+            //uhmmm uh add whatever key the user inputted here 
+            break;
+        }
+        default:{
+            console.log("Invalid selection - no key made");
+            break;
+        }
+    }
+    
 }
 
 function input(){
@@ -24,7 +40,7 @@ function input(){
             break;
         }
         default: { 
-            console.log("Input a proper number - Aborting..."); 
+            console.log("Ivalid selection - Aborting..."); 
             break; 
         }
     }
