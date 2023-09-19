fd3d1590850484e869ca40fea1313a6373119c2a
mcrangler37 <144483135+mcrangler37@users.noreply.github.com>
2023-09-19 13:44:01 -0500

    Update affineStuffBonus.ts
    made thing :3

--- affineStuffBonus.ts
+++ affineStuffBonus.ts
@@ -4,8 +4,8 @@ let numLetters:number = 26;
 let key:number[] = [0, 0];
 let userInput:any| null;
 let selection:unknown | null;
-let keyDigit1:number = Math.random() * 8 + 2;
-let keyDigit2:number = Math.random() * 99 + 1;
+let keyDigit1:number = Math.floor(Math.random() * 10) + 1;
+let keyDigit2:number = Math.floor(Math.random() * 100) + 1;
 let userKey:any = null;
 let keyIsValid:boolean = true;
 //Fx + S, where x is the value of the letter in the input
@@ -20,8 +20,12 @@ function getUserInput(){
 
     selection = prompt("Enter 1 for Encryption or 2 for Decryption");
     if(selection == 2){
-        userKey = prompt("Enter your decryption key, typed like ABB, A is digit 1 and B is digit 2");
+        userKey = prompt("Enter your decryption key");
         //check to see if key is valid, if it is keep going and if its not then set keyIsValid to false
+        if(!((userKey.substring(0, (userKey.indexOf(" "))) >= 1 && userKey.substring(0, (userKey.indexOf(" "))) <= 10) && userKey.substring(userKey.indexOf(" ")) >= 1 && userKey.substring(userKey.indexOf(" ")) <= 100)){
+            //if invalid
+        }
+        //if valid
     }
     keyGen();
     input();
