d0edfdb9341da7d857ee2bc964fab3ec60751c41
mcrangler37 <144483135+mcrangler37@users.noreply.github.com>
2023-09-20 21:49:28 -0500

    works

--- affine2.ts
+++ affine2.ts
@@ -1,17 +1,14 @@
 //setup variables :3
+let aVals:number[] = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25];
 let alphabetLetters:string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let key:number[] = [0, 0];
 let userInput:any = null;
 let selection:unknown | null;
-let a:number = Math.floor(Math.random() * 10) + 1;
-let b:number = Math.floor(Math.random() * 100) + 1;
+let a:number = aVals[Math.floor(Math.random() * 12)];
+let b:number = Math.floor(Math.random() * 26);
 let userKey:any = null;
 let keyIsValid:boolean = true;
 //ax + b % 26, where x is the value of the letter in the input
-
-if((a % 2) == 0){
-    a += 1;
-}
 
 //function calls
 createInput();
@@ -27,7 +24,7 @@ function getUserInput(){
         if(selection == 2){
             userKey = prompt("Enter your decryption key");
             //check to see if key is valid, if it is keep going and if its not then set keyIsValid to false
-            if(!((userKey.substring(0, (userKey.indexOf(" "))) >= 1 && userKey.substring(0, (userKey.indexOf(" "))) <= 10) && userKey.substring(userKey.indexOf(" ")) >= 1 && userKey.substring(userKey.indexOf(" ")) <= 100)){
+            if(!((userKey.substring(0, (userKey.indexOf(" "))) >= 1 && userKey.substring(0, (userKey.indexOf(" "))) <= 25) && userKey.substring(userKey.indexOf(" ") + 1) >= 1 && userKey.substring(userKey.indexOf(" ") + 1) <= 25)){
                 //if invalid
                 console.log("invalid decryption key - restarting...");
                 getUserInput();
@@ -56,19 +53,42 @@ function isInputValid(): any {
 
 
 function encrypt(){
-    //encrypt it lol
     let encryptedInput:string = "";
     for(let i = 0; i < userInput.length; i++){
-        //find number for original letter and plug in
-        let num:number = (a * (alphabetLetters.indexOf(userInput[i])) + b) % 26;
-        //find new letter from alphabet and add to encryptedString
-        encryptedInput += alphabetLetters[num];
+        if(userInput[i] == " "){
+            encryptedInput += userInput[i];
+        } else {
+            let num:number = (a * (alphabetLetters.indexOf(userInput[i])) + b) % 26;
+            encryptedInput += alphabetLetters[num];
+        }
     }
     console.log("Encrypted String: " + encryptedInput);
     console.log("Encryption Key: " + a + " " + b);
 }
 
 function decrypt(){
-    //decrypt it lol
-    
+    let decryptedInput:string = "";
+    let aInverse:number = 0;
+    let aInput:number = userKey.substring(0, (userKey.indexOf(" ")));
+    let bInput:number = userKey.substring((userKey.indexOf(" ") + 1));
+
+    for (let i = 0; i < 26; i++){
+        if ((aInput * i) % 26 == 1){
+            aInverse = i;
+        }
+    }
+
+    let bPrime:number = ((26 - bInput) * aInverse) % 26;
+
+    console.log(userKey);
+    console.log(aInverse + " " + bPrime);
+
+    for(let i = 0; i < userInput.length; i++){
+        let temp = alphabetLetters.indexOf(userInput[i]);
+        temp *= aInverse;
+        temp += bPrime;
+        temp %= 26;
+        decryptedInput += alphabetLetters[temp];
+    }
+    console.log("Decrypted String: " + decryptedInput);
 }
