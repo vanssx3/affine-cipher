31d5010f2f7e5f249a3b0caaf95406bdf4755e3c
mcrangler37 <144483135+mcrangler37@users.noreply.github.com>
2023-09-21 08:48:02 -0500

    Update and rename affine2.ts to cipherFinal.ts
    made dumdum changes

--- cipherFinal.ts
+++ cipherFinal.ts
@@ -1,20 +1,20 @@
 //setup variables :3
-let aVals:number[] = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25];
-let alphabetLetters:string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let alphabetLetters:string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-"];
 let key:number[] = [0, 0];
 let userInput:any = null;
 let selection:unknown | null;
-let a:number = aVals[Math.floor(Math.random() * 12)];
-let b:number = Math.floor(Math.random() * 26);
+let a:number = Math.floor(Math.random() * 37);
+let b:number = (Math.floor(Math.random() * 35) + 1);
 let userKey:any = null;
-//ax + b % 26, where x is the value of the letter in the input
+//ax + b % m, where x is the value of the letter in the input and m is the length of the alphabet
 
 //function calls
 getUserInput();
 
 //prompt user for string
 function createInput(){
-    userInput = prompt("Enter a string to be encrypted or decrypted (letters only)");
+    userInput = prompt("Enter a string to be encrypted or decrypted (numbers, letters, and - acceptable)");
+    userInput = userInput.toLowerCase();
 }
 
 //ask user to encrypt or decrypt, checks key validity
@@ -24,7 +24,7 @@ function getUserInput(){
         selection = prompt("Enter 1 for encryption or 2 for decryption");
         if(selection == 2){
             userKey = prompt("Enter the given encryption key for your string");
-            if(!((userKey.substring(0, (userKey.indexOf(" "))) >= 1 && userKey.substring(0, (userKey.indexOf(" "))) <= 25) && userKey.substring(userKey.indexOf(" ") + 1) >= 1 && userKey.substring(userKey.indexOf(" ") + 1) <= 25)){
+            if(!((userKey.substring(0, (userKey.indexOf(" "))) >= 1 && userKey.substring(0, (userKey.indexOf(" "))) <= (36)) && userKey.substring(userKey.indexOf(" ") + 1) >= 1 && userKey.substring(userKey.indexOf(" ") + 1) <= (36))){
                 //if invalid
                 console.log("invalid encryption key - restarting...");
                 setTimeout(() => {getUserInput()}, 500);
@@ -61,10 +61,11 @@ function encrypt(){
         if(userInput[i] == " "){
             encryptedInput += userInput[i];
         } else {
-            let num:number = (a * (alphabetLetters.indexOf(userInput[i])) + b) % 26;
+            let num:number = (a * (alphabetLetters.indexOf(userInput[i])) + b) % 37;
             encryptedInput += alphabetLetters[num];
         }
     }
+    console.log("Original Input: " + userInput);
     console.log("Encrypted String: " + encryptedInput);
     console.log("Encryption Key: " + a + " " + b);
 }
@@ -76,13 +77,13 @@ function decrypt(){
     let aInput:number = userKey.substring(0, (userKey.indexOf(" ")));
     let bInput:number = userKey.substring((userKey.indexOf(" ") + 1));
 
-    for (let i = 0; i < 26; i++){
-        if ((aInput * i) % 26 == 1){
+    for (let i = 0; i < 37; i++){
+        if ((aInput * i) % 37 == 1){
             aInverse = i;
         }
     }
 
-    let bPrime:number = ((26 - bInput) * aInverse) % 26;
+    let bPrime:number = ((37 - bInput) * aInverse) % 37;
 
     console.log("Decryption Key: " + aInverse + " " + bPrime);
 
@@ -93,9 +94,10 @@ function decrypt(){
             let temp = alphabetLetters.indexOf(userInput[i]);
             temp *= aInverse;
             temp += bPrime;
-            temp %= 26;
+            temp %= 37;
             decryptedInput += alphabetLetters[temp];
         }
     }
+    console.log("Original String: " + userInput);
     console.log("Decrypted String: " + decryptedInput);
 }
